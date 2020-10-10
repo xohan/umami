40e4b0cc56138ee97ab36d99bf21eb27925f230b
xohan <xohansosa@gmail.com>
2020-10-10 04:29:52 -0300

    a

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const pkg = require('./package.json');
 module.exports = {
   env: {
     VERSION: pkg.version,
-    FORCE_SSL: !!process.env.FORCE_SSL,
+    FORCE_SSL: !!process.env.FORCE_SSL
   },
   webpack(config) {
     config.module.rules.push({
